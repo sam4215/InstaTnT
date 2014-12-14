e4bb15de8ff94e26b153fff0683126d33426ccc4
TheEpicButterStudios <samuel.bird4215@gmail.com>
2014-12-13 19:21:11 -0600

    Initial commit

---
+++ src/me/ButterDev/InstaTnT/InstaTnT.java
@@ -0,0 +1,19 @@
+package me.ButterDev.InstaTnT;
+
+/**
+ * Created by Sam (ButterDev) on 12/13/2014.
+ */
+
+import org.spongepowered.api.event.state.ServerStartingEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+/**
+ * This is the main class for the InstaTnT Sponge plugin. This is a port from dev.bukkit.org/bukkit-plugins/instatnt/
+ */
+@Plugin(name="InstaTnT by ButterDev", id="InstaTnT")
+public class InstaTnT {
+    public void onServerEnable(ServerStartingEvent event)
+    {
+
+    }
+}
